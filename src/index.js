0a54a3437a3b949f87f442e5a68b3b15133c7157
Luis <lar_dev@icloud.com>
2024-03-14 18:42:33 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import EventSearchComponent from "./components/eventSearchComponent";
+
+const App = () => (
+  <>
+    <EventSearchComponent />
+  </>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
