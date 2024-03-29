42d96ee21f0c17ac9feb559ffbdfcd65e49c45f9
Luis <lar_dev@icloud.com>
2024-03-29 18:27:38 +0100

    bundled comp

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import EventSearchComponent from "./components/eventSearchComponent";
-import EventDetailsComponent from "./components/eventDetailsComponent";
 
 const App = () => (
   <>
     <EventSearchComponent />
-    {/* <EventDetailsComponent /> */}
   </>
 );
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("event-list"));
 root.render(<App />);
